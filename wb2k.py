6c92bf598a2ac1a0ab3a360262bea48ac5fed86e
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2019-06-21 00:43:37 -0700

    Remove poor "layabout" functionality

--- wb2k.py
+++ wb2k.py
@@ -1,10 +1,5 @@
-import os
-import logging
-
 import click
-from slackclient import SlackClient
-
-from layabout import setup_logging, run
+import slack
 
 __author__ = 'Reilly Tucker Siemens'
 __email__ = 'reilly@tuckersiemens.com'
@@ -25,31 +20,7 @@ __version__ = '0.2.0-what-did-you-expect'
               help='The maximum number of times to attempt to reconnect on websocket connection errors')
 @click.version_option()
 def cli(channel: str, message: str, verbose: int, retries: int) -> None:
-    # Due to some unfortunate limitations in the Slack RTM API
-    # (https://api.slack.com/rtm#formatting_messages) message formatting is
-    # limited to basic formatting.
-
-    if verbose > 11:
-        sys.exit(bail('fatal', 'red', "It doesn't go beyond 11"))
-
-    # Get our logging in order.
-    logger = logging.getLogger(__name__)
-    setup_logging(verbose)
-
-    # Make sure we have an API token.
-    api_token = os.environ.get('WB2K_TOKEN')
-    if not api_token:
-        sys.exit(bail('fatal', 'red', 'WB2K_TOKEN envvar undefined'))
-
-    sc = SlackClient(api_token)
-
-    run(
-        sc=sc,
-        channel=channel,
-        message=message,
-        retries=retries,
-        logger=logger
-    )
+    raise click.ClickException(click.style("It doesn't work.", fg='red'))
 
 if __name__ == '__main__':
     cli()
