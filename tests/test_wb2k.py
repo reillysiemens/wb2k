d75696ea6cc891fc1d04ef694be08140d02d33cb
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2019-06-25 21:28:37 -0700

    🎸 It doesn't go beyond 11

--- tests/test_wb2k.py
+++ tests/test_wb2k.py
@@ -1,5 +1,23 @@
-import wb2k  # noqa: Required to generate coverage.
+import pytest
+from click.testing import CliRunner
+
+from wb2k import cli
 
 
-def test_wb2k():
-    pass
+@pytest.fixture()
+def runner():
+    return CliRunner()
+
+
+@pytest.mark.parametrize("verbosity", ["-" + "v" * n for n in range(1, 12)])
+def test_it_goes_up_to_11(runner, verbosity):
+    """ wb2k's verbose option can be repeated up to 11 times. """
+    result = runner.invoke(cli, [verbosity])
+    assert result.exit_code == 0
+
+
+def test_it_doesnt_go_beyond_11(runner):
+    """ wb2k's verbose option cannot be repeated more than 11 times. """
+    result = runner.invoke(cli, ["-vvvvvvvvvvvv"])
+    assert result.output == "Error: It doesn't go beyond 11\n"
+    assert result.exit_code == 1
--- wb2k.py
+++ wb2k.py
@@ -9,4 +9,6 @@ __version__ = "0.2.0-what-did-you-expect"
 @click.option("-v", "--verbose", count=True, help="It goes to 11.")
 @click.version_option()
 def cli(verbose: int) -> None:
-    raise click.ClickException(click.style("It doesn't work.", fg="red"))
+    """ TODO """
+    if verbose > 11:
+        raise click.ClickException("It doesn't go beyond 11")
